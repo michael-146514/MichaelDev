4139bee125369176d3498a0e6738a09c686f3afa
hereMc <santamariamichael5@gmail.com>
2023-08-27 23:47:17 -0500

    First commit to Github

---
+++ app.js
@@ -0,0 +1,16 @@
+const obserber = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+console.log(entry)
+    if(entry.isIntersecting){
+entry.target.classList.add('show');
+    }else{
+        entry.target.classList.remove('show');
+    }
+
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => obserber.observe(el));
+
+
